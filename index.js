2b3d2d3e6059cb2bd1f4b3f9f84fcf7057e4e63b
mohammadgithub2326 <132339501+mohammadgithub2326@users.noreply.github.com>
2023-07-23 10:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
-const cards = [
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2018/12/kirkjufell.jpg',
-        imgAlt: 'Kirkjufell',
-        header: 'Kirkjufell',
-        description: 'Park up in the little seaside town of Grundarfjörður and follow the hour-and-a-half hiking trail to Kirkjufell’s summit.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2018/12/table-mountain-1170x731.jpg',
-        imgAlt: 'Table Mountain',
-        header: 'Table Mountain',
-        description: 'One of the world’s most famous pinnacles, Table Mountain watches over the South African city of Cape Town from 1,085 metres at its highest point.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/mount-huangshan-1170x780.jpg',
-        imgAlt: 'Mount Huangshan (Yellow Mountain)',
-        header: 'Mount Huangshan',
-        description: 'The mountain resembles a turreted fortress, perched ominously above the spruce and fir trees in the Canadian Rockies.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2018/12/matterhorn-1170x780.jpg',
-        imgAlt: 'Matterhorn',
-        header: 'Matterhorn',
-        description: 'This Alpine peak spans the border between Switzerland and Italy. At 4,478 metres it’s smaller than the famed Mont Blanc (4,810 metres).',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/mount-fuji-1170x780.jpg',
-        imgAlt: 'Mount Fuji',
-        header: 'Mount Fuji',
-        description: 'If you’re down for the hike, choose between one of the four trails that lace the mountain. The Yoshida trail is most popular choice and buses whisk you right from Tokyo to the trailhead.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/mount-cook-1170x778.jpg',
-        imgAlt: 'Aoraki/Mount Cook',
-        header: 'Aoraki/Mount Cook',
-        description: 'The name Mount Cook was given to the peak by European settlers. But native peoples had always known it as Aoraki.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/mount-everest-1170x780.jpg',
-        imgAlt: 'Mount Everest',
-        header: 'Mount Everest',
-        description: 'The mother of all mountains, she rises to more than 8,848 metres, jutting out of the Himalayas, on the Nepal-Tibet border.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/kilimanjaro-1170x783.jpg',
-        imgAlt: 'Kilimanjaro',
-        header: 'Kilimanjaro',
-        description: 'Kilimanjaro is actually a volcano made up of a trio of peaks. Though if you’re planning a trip, you’ll be glad to hear it’s dormant – the last eruption happened some 360,000 years ago.',
-    },
-    {
-        imgUrl: 'https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/01/cradle-1170x720.jpg',
-        imgAlt: 'Cradle Mountain',
-        header: 'Cradle Mountain',
-        description: 'It’s one of Tasmania’s most popular tourist attractions and is around an hour-and-a-half drive from the busy city of Devonport.',
-    },
-]
+console.clear();
+
+let temp_clothing_container = document.createElement("div");
+temp_clothing_container.classList.add("row", "justify-center", "card_container");
+
+let temp_accessories_container = document.createElement("div");
+temp_accessories_container.classList.add("row", "justify-center", "card_container");
+
+for(let i = 0; i < productList.length; i++)
+{
+	let temp_card, temp_element;
+
+	// create card element
+	temp_card = document.createElement("div");
+	temp_card.classList.add("card", "row");
+	if(productList[i]["isAccessory"]) temp_card.id = "accessories_" + productList[i]["id"];
+	else temp_card.id = "clothing_" + productList[i]["id"];
+
+	// card link - a
+	temp_element = document.createElement("a");
+	temp_element.classList.add("column");
+	temp_element.href = "product.html?product_id=" + productList[i]["id"];
+	temp_card.appendChild(temp_element);
+
+	// image container div
+	temp_element = document.createElement("div");
+	temp_element.classList.add("image");
+	temp_card.querySelector("a").appendChild(temp_element);
+
+	// image
+	temp_element = document.createElement("img");
+	temp_element.src = productList[i]["preview"];
+	temp_card.querySelector(".image").appendChild(temp_element);
+
+	// details container div
+	temp_element = document.createElement("div");
+	temp_element.classList.add("column", "details");
+	temp_card.querySelector("a").appendChild(temp_element);
+
+	// card title h3
+	temp_element = document.createElement("h3");
+	temp_element.classList.add("title");
+	temp_element.appendChild(document.createTextNode(productList[i]["name"]));
+	temp_card.querySelector(".details").appendChild(temp_element);
+
+	// card subtitle h4
+	temp_element = document.createElement("h4");
+	temp_element.classList.add("subtitle");
+	temp_element.appendChild(document.createTextNode(productList[i]["brand"]));
+	temp_card.querySelector(".details").appendChild(temp_element);
+
+	// card price h5
+	temp_element = document.createElement("h5");
+	temp_element.classList.add("price");
+	temp_element.appendChild(document.createTextNode("Rs " + productList[i]["price"]));
+	temp_card.querySelector(".details").appendChild(temp_element);
+
+	console.log(temp_card);
+
+	if(productList[i]["isAccessory"]) temp_accessories_container.appendChild(temp_card);
+	else temp_clothing_container.appendChild(temp_card);
+}
+
+document.getElementById("clothing").appendChild(temp_clothing_container);
+document.getElementById("accessories").appendChild(temp_accessories_container);
+
+
+/*
+	sample html code for card
+
+	<div class="card row" id="1">
+		<a class="column" href="product.html?product_id=1">
+			<div class="image">
+				<img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg">
+			</div>
+			<div class="column details">
+				<h3 class="title">Men Navy Blue Solid Sweatshirt</h3>
+				<h4 class="subtitle">United Colors of Benetton</h4>
+				<h5 class="price">Rs 2599</h5>
+			</div>
+		</a>
+	</div>
+*/
